eeefe85e5b6f4a202588096c055a18c7b683bf15
mevol <melanie.vollmar@diamond.ac.uk>
2021-06-23 16:29:17 +0100

    and another try to use XYZ limits

--- modules/cnn/prepare_training_data_binary.py
+++ modules/cnn/prepare_training_data_binary.py
@@ -80,7 +80,10 @@ def prepare_training_data_binary(
           box = gemmi.FractionalBox()
           box.minimum = gemmi.Fractional(0, 0, 0)
           box.maximum = map_to_map.grid.unit_cell.fractionalize(upper_limit)
-          box.maximum = map_to_map.grid.point_to_fractional(map_to_map.grid.get_point(tuple(xyz_limits)))
+          box.maximum = map_to_map.grid.point_to_fractional(
+              map_to_map.grid.get_point(int(xyz_limits[0]),
+                                        int(xyz_limits[1]),
+                                        int(xyz_limits[2])))
           #box.maximum = map_to_map.grid.point_to_fractional(map_to_map.grid.get_point(100, 100, 100))
           #box.maximum = map_to_map.grid.point_to_fractional(map_to_map.grid.get_point(50, 50, 50))
           box.add_margin(1e-5)
