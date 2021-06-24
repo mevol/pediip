2e54b97210d95b60a2f353c53a8e282ff7d8b587
mevol <melanie.vollmar@diamond.ac.uk>
2021-06-24 19:16:55 +0100

    and again

--- modules/cnn/training_models/training_pipeline_binary2D.py
+++ modules/cnn/training_models/training_pipeline_binary2D.py
@@ -141,7 +141,7 @@ def pipeline(create_model: Callable[[int, int, int], Model], parameters_dict: di
     for name in names:
       print("Image name ", name)
       sample = data.loc[data["file_path"].str.contains(name)]
-      print(sample.values)
+      print(sample.values[-1])
 #      print(sample.iloc[-1])
 #      print(sample.iloc[-1].values)
 #      if name in data["file_path"]:
