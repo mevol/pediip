83d49daf306a099d362fd00c3da93b0129b518c8
mevol <melanie.vollmar@diamond.ac.uk>
2021-06-24 19:12:41 +0100

    another try to get just the value

--- modules/cnn/training_models/training_pipeline_binary2D.py
+++ modules/cnn/training_models/training_pipeline_binary2D.py
@@ -143,6 +143,7 @@ def pipeline(create_model: Callable[[int, int, int], Model], parameters_dict: di
       sample = data.loc[data["file_path"].str.contains(name)]
       print(sample)
       print(sample.iloc[-1])
+      print(sample.iloc[-1].value)
 #      if name in data["file_path"]:
 #        print(data.index)
         
