3196508f67d807fa5b3a601f61d2f1afc29231c9
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-01 15:46:44 +0000

    increased the line number to cache to see if this makes any difference to extract multiple clusters; at the moment it would seem that all prosmart results consist of single clusters

--- modules/create_mr_set/db/pdb_mr_ssm_parser.py
+++ modules/create_mr_set/db/pdb_mr_ssm_parser.py
@@ -323,7 +323,7 @@ class MRSSMParser(object):
             mean_cos_theta = split[2]
             sd_cos_theta = split[3]
 #            testme = list(islice(pro_log, 2))
-            print(getline(pro_log.name, ind + 4))
+            print(getline(pro_log.name, ind + 10))
     else:
       procrustes = 0
       flexible = 0
