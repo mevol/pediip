15b21486ccea65d59597e063f5cd07cb47b04263
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-01 15:05:17 +0000

    another try to fix out of range error

--- modules/create_mr_set/db/pdb_mr_ssm_parser.py
+++ modules/create_mr_set/db/pdb_mr_ssm_parser.py
@@ -314,10 +314,11 @@ class MRSSMParser(object):
             flexible = result1[1].split()[-1]
           if line.strip().startswith("Final clustering results:"):
             print(list(islice(pro_log, 2)))
-            result2 = list(islice(pro_log, 2))
+            print(list(islice(pro_log, 2))[1])
+            result2 = list(islice(pro_log, 2)[1])
             print(result2)
-            print(result2[1])
-            print(result2[1].split())
+#            print(result2[1])
+#            print(result2[1].split())
             cluster = result2[0]
             fragments = result2[1]
             mean_cos_theta = result2[2]
