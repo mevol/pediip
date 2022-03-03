2b96d5c8b00e86762fe9908d27dbded4ddc7d619
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-03 14:03:03 +0000

    pdb-redo work 24

--- modules/create_mr_set/db/pdb_redo_parser.py
+++ modules/create_mr_set/db/pdb_redo_parser.py
@@ -39,8 +39,18 @@ class PDBRedo(object):
     with open(filename, "r") as data_file:
       line = next((l for l in data_file if structure.lower() in l), None)
       print(line)
+      split = line.split()
+      print(split)
+      rwork_deposited = sample.split()[2]
+      rfree_deposited = sample.split()[3]
+      rwork_tls = sample.split()[9]
+      rfree_tls = sample.split()[10]
+      rwork_final = sample.split()[14]
+      rfree_final = sample.split()[15]
+      completeness = sample.split()[-21]
       
-      
+      print(rwork_deposited, rfree_deposited, rwork_tls, rfree_tls, rwork_final,
+            rfree_final, completeness)
       
 #      data = data_file.readlines()
 #      print(len(data))
@@ -61,16 +71,6 @@ class PDBRedo(object):
 #        if not line.strip().startswith("#"):
 
 #          print(line)
-        split = line.split()
-        print(split)
-        structure_id = sample.split()[0].upper()
-        rwork_deposited = sample.split()[2]
-        rfree_deposited = sample.split()[3]
-        rwork_tls = sample.split()[9]
-        rfree_tls = sample.split()[10]
-        rwork_final = sample.split()[14]
-        rfree_final = sample.split()[15]
-        completeness = sample.split()[-21]
     
       cur.executescript( '''
           INSERT OR IGNORE INTO pdb_id
