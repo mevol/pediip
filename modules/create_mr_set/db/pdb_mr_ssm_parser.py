002763fa9a821fa7cb5f22af0da5cbc60b2535f5
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-09 20:47:11 +0000

    adding else to catch missing values in database

--- modules/create_mr_set/db/pdb_mr_ssm_parser.py
+++ modules/create_mr_set/db/pdb_mr_ssm_parser.py
@@ -593,6 +593,8 @@ class MRSSMParser(object):
           f_map_correlation_afterMR0 = h_reader["f_map_correlation_afterMR0"]
         if "num_fragments_afterMolrep0" in h_reader:
           num_fragments_afterMR0 = h_reader["num_fragments_afterMR0"]
+        else:
+          num_fragments_afterMR0 = 0
         if "num_res_built_afterMR0" in h_reader:
           num_res_built_afterMR0 = h_reader["num_res_built_afterMR0"]
         if "num_res_sequenced_afterMR0" in h_reader:
