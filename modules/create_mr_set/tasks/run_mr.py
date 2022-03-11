b808d70a436f1f25f97e70818640298080301cbc
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-11 18:18:45 +0000

    another attempt to control the different if statments and when to stop them

--- modules/create_mr_set/tasks/run_mr.py
+++ modules/create_mr_set/tasks/run_mr.py
@@ -686,8 +686,7 @@ def run_mr_pipelines(key, homologue, args):
       write_combined_mtz_afterMR_buccaneer_jelly(key, homologue, args)
       compare_phases_afterMR_buccaneer_jelly(key, homologue, args)
     break
-    elif:
-      os.path.exists(homologue.path("refmac_afterMolrep.mtz"))
+    if os.path.exists(homologue.path("refmac_afterMolrep.mtz")):
       # Molrep-placed model after 100 cycles jelly body Refmac refinement; built with Buccaneer;
       # refined with Refmac 0-cycle and 100 cycles jelly body using the PDB-redo ground truth
       # MTZ;
@@ -703,8 +702,7 @@ def run_mr_pipelines(key, homologue, args):
       write_combined_mtz_afterMolrep_buccaneer_jelly(key, homologue, args)
       compare_phases_afterMolrep_buccaneer_jelly(key, homologue, args)
     break
-    elif:
-      os.path.exists(homologue.path("refmac_afterSSM.mtz"))
+    if os.path.exists(homologue.path("refmac_afterSSM.mtz")):
       # Prosmart-placed model after 100 cycles jelly body Refmac refinement; built with Buccaneer;
       # refined with Refmac 0-cycle and 100 cycles jelly body using the PDB-redo ground truth
       # MTZ;
