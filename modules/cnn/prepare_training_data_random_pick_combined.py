cc19733842775c758485193cc24f561e94a4f1da
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-22 14:34:15 +0000

    another try to open file; this time keep /dls

--- modules/cnn/prepare_training_data_random_pick_combined.py
+++ modules/cnn/prepare_training_data_random_pick_combined.py
@@ -147,7 +147,7 @@ def prepare_training_data_random_pick_combined(
                 pass
             # Check path to map exists
             try:
-                map_file_path = Path(real_path_to_map_opt)
+                map_file_path = Path(real_path_to_map)
                 print(map_file_path)
 #                assert map_file_path.exists()
 #                assert os.path.exists(map_file_path)
