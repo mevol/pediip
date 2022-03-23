872a6b36449d0bd4e5bc1aa547741a68800469f4
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-23 13:57:58 +0000

    data generator is imported but need to get it working properly now

--- modules/cnn/training_models/data_generator_binary_rfree.py
+++ modules/cnn/training_models/data_generator_binary_rfree.py
@@ -26,7 +26,6 @@ class DataGenerator(Sequence):
 
   def __len__(self):
     'Denotes the number of batches per epoch'
-#    return int(np.floor(len(self.list_IDs.keys()) / self.batch_size))
     return int(np.floor(len(self.list_IDs) / self.batch_size))
 
   def __getitem__(self, index):
@@ -35,27 +34,11 @@ class DataGenerator(Sequence):
     #print("All indexes ", index)
     indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
     #print("Indexes of batch ", indexes)
-
     # Find list of IDs
-    #list_IDs_temp = [self.list_IDs[k] for k in indexes]
     list_IDs_temp = [self.iterator[k] for k in indexes]
-    
-#    list_IDs_temp = []
-#    for k in indexes:
-      #print("Index ", k)
-#      print("List of IDs ", self.list_IDs)
-      #print("ID for index k ", self.iterator[k])
-#      list_IDs_temp.append(self.iterator[k])
-    
     #print("List of IDs ", self.iterator)
-
-#    list_labels_temp = [self.labels[k] for k in indexes]
-
     # Generate data
-#    X, y = self.__data_generation(list_IDs_temp, list_labels_temp)
     X, y = self.__data_generation(list_IDs_temp)
-
-
     return X, y
 
   def on_epoch_end(self):
@@ -65,24 +48,12 @@ class DataGenerator(Sequence):
     if self.shuffle == True:
       np.random.shuffle(self.indexes)
 
-#  def __data_generation(self, list_IDs_temp, list_labels_temp):
   def __data_generation(self, list_IDs_temp):
     'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
     # Initialization
-    #X = np.empty([self.batch_size, *self.dim])
     X = np.empty((self.batch_size, *self.dim, self.n_channels))
-
-    #print("Empty X", X.shape)
-    #below creates an empty np.array; [0 0 0 0]
-    #y = np.empty([1, 4])
-    #print("Empty y", y.shape)
-
-    #print(len(list_IDs_temp))
-#    print(len(list_labels_temp))
-
     #this version was used in python3-topaz
     y = np.empty([self.batch_size], dtype=int)
-    #print("Empty y", y.shape)
     
     # Generate data
 
@@ -97,12 +68,12 @@ class DataGenerator(Sequence):
 
 
     for i, ID in enumerate(list_IDs_temp):
-      #print(777, ID)#is the path to the ccp4 map to open
-      #print(i)#is the index within the chosen batch size
+      print(777, ID)#is the path to the ccp4 map to open
+      print(i)#is the index within the chosen batch size
     
       #print("Full list of IDs", self.list_IDs)
 
-#      print(99999999999999, X[i,])
+      print(99999999999999, X[i,])
  
 #      with mrcfile.open(self.list_IDs[ID]) as mrc:
 #        volume = mrc.data
