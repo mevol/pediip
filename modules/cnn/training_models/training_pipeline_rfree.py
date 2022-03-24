c573d892dcf8286b9ba7203ca1cf4a4e1548da6d
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-24 15:57:46 +0000

    fixing unassigned variable

--- modules/cnn/training_models/training_pipeline_rfree.py
+++ modules/cnn/training_models/training_pipeline_rfree.py
@@ -231,15 +231,25 @@ def pipeline(create_model: Callable[[int, int, int], Model], parameters_dict: di
                                       n_classes=2,
                                       shuffle=False)#was True
 
-    history = model.fit_generator(
-             training_generator,
-             steps_per_epoch=int((len(active_training_set["Files"]) / batch_size)),
-             epochs=epochs,
-             validation_data=testing_generator,
-             validation_steps=(len(active_validation_set["Files"]) / batch_size),
-             use_multiprocessing=True,
-             workers=8,
-         )
+    history = model.fit(
+        training_generator,
+        steps_per_epoch=int((len(X_train) / batch_size)),#len(X) if not using train-test-split
+        epochs=epochs,
+        validation_data=testing_generator,
+        validation_steps=(len(X_test) / batch_size),
+        use_multiprocessing=True,
+        workers=8)#8)
+
+
+#    history = model.fit_generator(
+#             training_generator,
+#             steps_per_epoch=int((len(active_training_set["Files"]) / batch_size)),
+#             epochs=epochs,
+#             validation_data=testing_generator,
+#             validation_steps=(len(active_validation_set["Files"]) / batch_size),
+#             use_multiprocessing=True,
+#             workers=8,
+#         )
 
 # TO DO: This should go into the data generator; probably need to do a new one
 #    prepare_training_data_random_pick_combined(parameters_dict["sample_lable_lst"],
