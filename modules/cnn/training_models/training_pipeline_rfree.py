cf132bf656a7f78f5632fff3eb48d4957d33fc02
mevol <melanie.vollmar@diamond.ac.uk>
2022-03-28 21:09:56 +0100

    another try

--- modules/cnn/training_models/training_pipeline_rfree.py
+++ modules/cnn/training_models/training_pipeline_rfree.py
@@ -171,10 +171,9 @@ def pipeline(create_model: Callable[[int, int, int, int], Model], parameters_dic
     last_y = y_test.iloc[-1]
     print(last_y)
     additional_y = pd.DataFrame(np.repeat(last_y, diff_batch_samples, axis=0))#last.values
+    print(len(additional_y))
     additional_y_dict = additional_y.to_dict()
     extended_y_test = label_dict.update(additional_y_dict)
-    
-    print(len(extended_y_test))
     
     # set input dimensions for images and number of channels based on whether color or
     # grayscale is used
