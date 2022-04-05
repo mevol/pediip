00805368356fc5b2536288c09c783ae7e2d35e05
mevol <melanie.vollmar@diamond.ac.uk>
2022-04-05 17:19:33 +0100

    adding a MLP to try as simple system

---
+++ modules/cnn/training_models/simple_3d_mlp_rfree.py
@@ -0,0 +1,34 @@
+"""Testing with training_pipeline"""
+
+from typing import Tuple
+
+#from keras import Sequential, optimizers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Conv3D, Dense, Dropout, Flatten, MaxPooling3D
+from tensorflow.keras.layers import GlobalAveragePooling3D, BatchNormalization
+
+from modules.cnn.training_models.training_pipeline_rfree import pipeline_from_command_line
+
+def create_3D_cnn_model(input_shape: Tuple[int, int, int, int]):
+    model = Sequential()
+
+    model.add(Dense(512, activation="relu", input_shape=input_shape))
+    model.add(Dense(2, activation="softmax"))
+
+    print(model.output_shape)
+
+
+    model.compile(
+        loss="categorical_crossentropy",
+#        optimizer=optimizers.adam(lr=1e-5),
+        optimizer='adam',
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+
+    pipeline_from_command_line(create_3D_cnn_model, rgb=False)
