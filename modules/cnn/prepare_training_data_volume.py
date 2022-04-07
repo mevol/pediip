1bc840159617d63c6183754ea621cc0507ce0d35
mevol <melanie.vollmar@diamond.ac.uk>
2022-04-07 18:03:23 +0100

    another manual try to rotate the volume around axis x

--- modules/cnn/prepare_training_data_volume.py
+++ modules/cnn/prepare_training_data_volume.py
@@ -211,10 +211,40 @@ def prepare_training_data_volume(
             data_to_map.set_extent(box)
             map_grid = data_to_map.grid
             map_array = np.array(map_grid, copy = False)
+
+            length = int(xyz_limits[0])+1
+            edited_volume = np.zeros((length,
+                                            length,
+                                            length))
+            print("empty edited volume: ", edited_volume.shape)
+            # Slice the volume into images
+            #image_slices, bytes = slice_map(map_array, slices_per_axis)
+            # Iterate through images, scale them and save them in output_directory
+            deg = np.random.choice(90, 1, replace=False)[0]
+            for slice_num in range(map_array.shape[0]):
+                #print("Working on slice number: ", slice_num)
+                # Get slice
+                slice = map_array[slice_num, :, :]
+                # Scale slice
+                slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
+                # Round to the nearest integer
+                slice_scaled_int = np.rint(slice_scaled)
+                # do data augmentation as rotation for a random angle between 0 and 90 deg
+                # for all even numbers in the total image stack
+                # check that the remainder of division is 0 and hence the result even
+                # get a random number between 0 and 90 deg
+                # rotate the slice by this deg
+                slice_scaled_int = rotate(slice_scaled_int, angle = deg, reshape=False)
+                # combine the slices to a new image stack for training
+                edited_volume[slice_num, :, :] = slice_scaled_int
+            print("filled edited volume: ", edited_volume.shape)
+
+
+
 #            rotated_volume = rotation(map_array)
 
 #            rotated_volume = rotate_man(map_array, 20, x)
-            rotated_volume = rotate(map_array)
+#            rotated_volume = rotate(map_array)
 
             logging.info(f"Size of standardise map when finished: {map_array.shape} \n")
         except Exception:
@@ -231,8 +261,8 @@ def prepare_training_data_volume(
         logging.error(f"Could not open input map list \n")
         raise
 #    return map_array
-    return rotated_volume
-
+#    return rotated_volume
+    return edited_volume
 
 
 def params_from_yaml(args):
