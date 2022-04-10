371529a07c711ea62090c4c89dbebb5910f11a36
mevol <melanie.vollmar@diamond.ac.uk>
2022-04-10 09:22:45 +0100

    fixing typo in argument name

--- modules/cnn/prepare_training_data_random_pick_combined.py
+++ modules/cnn/prepare_training_data_random_pick_combined.py
@@ -15,7 +15,7 @@ from sys import getsizeof
 from scipy import ndimage
 
 
-def slice_map(volume, slices_per_axis, augentation):
+def slice_map(volume, slices_per_axis, augmentation):
     """Slice the volume into 2d panes along x, y, z axes and return as an image stack"""
     # Check volume is equal in all directions
     assert (
