48998644c018f08b36bc02d2cc0e150d5f40db3f
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-11 09:34:48 +0100

    fixed the wrong indentation

--- modules/cnn/training_models/training_pipeline_binary2D.py
+++ modules/cnn/training_models/training_pipeline_binary2D.py
@@ -217,11 +217,11 @@ def pipeline(create_model: Callable[[int, int, int], Model], parameters_dict: di
                              + len(partition['train'])
                              + len(partition['challenge']))
 
-    # expand X_train, X_test and X_validation from single MTZ to contain
-    # the corresponding 60 image slices
-    def expand_sets(X_set, y_set):
-      for sample in X_set:
-        print(sample.index)
+  # expand X_train, X_test and X_validation from single MTZ to contain
+  # the corresponding 60 image slices
+  def expand_sets(X_set, y_set):
+    for sample in X_set:
+      print(sample.index)
 
     1/0
 
