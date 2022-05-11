7885d81844f0a6e124a483069febcf4712de010a
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-11 16:22:49 +0100

    trying to normalise whole array rather than slices

--- modules/cnn/prepare_training_data_volume.py
+++ modules/cnn/prepare_training_data_volume.py
@@ -114,23 +114,31 @@ def prepare_training_data_volume(
       # Iterate through images, scale them and save them in output_directory
       deg = np.random.choice(90, 1, replace=False)[0]
       map_array = rotate(map_array, angle = deg, reshape=False)# see what happens with no rotation
+      
+      # normalize 3D array after rotation
+      Amax = np.max(map_array)
+      Amin = np.min(map_array)
+      Range = Amax - Amin
+      Anrm = ((map_array - Amin)/Range - 0.5) * 2
+      
+      
 #      for slice_num in range(map_array.shape[0]):
-      for slice_num in range(edited_volume.shape[0]):
-        #print("Working on slice number: ", slice_num)
-        # Get slice
-        slice = map_array[slice_num, :, :]
-        # Scale slice
-        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
-        # Round to the nearest integer
-        slice_scaled_int = np.rint(slice_scaled)
-        # do data augmentation as rotation for a random angle between 0 and 90 deg
-        # for all even numbers in the total image stack
-        # check that the remainder of division is 0 and hence the result even
-        # get a random number between 0 and 90 deg
-        # rotate the slice by this deg
-        #slice_scaled_int = rotate(slice_scaled_int, angle = deg, reshape=False)# see what happens with no rotation
-        # combine the slices to a new image stack for training
-        edited_volume[slice_num, :, :] = slice_scaled_int
+#      for slice_num in range(edited_volume.shape[0]):
+#        #print("Working on slice number: ", slice_num)
+#        # Get slice
+#        slice = map_array[slice_num, :, :]
+#        # Scale slice
+#        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
+#        # Round to the nearest integer
+#        slice_scaled_int = np.rint(slice_scaled)
+#        # do data augmentation as rotation for a random angle between 0 and 90 deg
+#        # for all even numbers in the total image stack
+#        # check that the remainder of division is 0 and hence the result even
+#        # get a random number between 0 and 90 deg
+#        # rotate the slice by this deg
+#        #slice_scaled_int = rotate(slice_scaled_int, angle = deg, reshape=False)# see what happens with no rotation
+#        # combine the slices to a new image stack for training
+#        edited_volume[slice_num, :, :] = slice_scaled_int
       logging.info(f"Size of standardise map when finished: {map_array.shape} \n")
     except Exception:
       logging.error(f"Could not expand map {map_file_path} \n")
@@ -138,7 +146,8 @@ def prepare_training_data_volume(
   except Exception:
     logging.error(f"Could not open input map list \n")
     raise
-  return edited_volume
+#  return edited_volume
+  return Anrm
 
 
 def params_from_yaml(args):
