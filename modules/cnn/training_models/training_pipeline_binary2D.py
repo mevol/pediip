2d3a1c13da1c69f35fdba68d2817fd215f7c3fb1
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-12 18:05:27 +0100

    try again

--- modules/cnn/training_models/training_pipeline_binary2D.py
+++ modules/cnn/training_models/training_pipeline_binary2D.py
@@ -231,11 +231,11 @@ def pipeline(create_model: Callable[[int, int, int], Model], parameters_dict: di
     df = pd.DataFrame(columns = ["filename", "ai_label"])
     #new_set = []
 #    for sample in names:
-    for row in temp:
-      print(row)
-      sample = row.loc[:, 'filename']
+    for i in temp:
+      print(i)
+      sample = temp.loc[i, 'filename']
       print(sample)
-      label = row.loc[:, 'ai_label']
+      label = temp.loc[i, 'ai_label']
       print(label)
       sample_split = sample.split('/')
       name = sample_split[-1]
