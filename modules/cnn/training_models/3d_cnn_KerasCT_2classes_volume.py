e8789bd1597d78a9c670fa6f41590a064267d0b7
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-17 13:02:53 +0100

    another import fix

--- modules/cnn/training_models/3d_cnn_KerasCT_2classes_volume.py
+++ modules/cnn/training_models/3d_cnn_KerasCT_2classes_volume.py
@@ -3,6 +3,7 @@
 from typing import Tuple
 
 from tensorflow import keras
+from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers import SGD
