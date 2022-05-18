01478e3722dc5e5fffaf9d01a16da6c68e8402be
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-18 15:43:23 +0100

    editing data generator to use mrc library to open ccp4 maps and do a simple rotation of XY plane

--- modules/cnn/training_models/data_generator_volume_mrc_classification.py
+++ modules/cnn/training_models/data_generator_volume_mrc_classification.py
@@ -5,7 +5,6 @@ from tensorflow.keras.utils import Sequence
 from tensorflow.keras.utils import to_categorical
 from scipy.ndimage import rotate
 
-#class DataGenerator(keras.utils.Sequence):
 class DataGenerator(Sequence):
   'Generates data for Keras'
   def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32),
@@ -56,70 +55,85 @@ class DataGenerator(Sequence):
 
     # Generate data
     for i, ID in enumerate(list_IDs_temp):
-      with mrcfile.open(self.list_IDs.iloc[ID, 0]) as mrc:
+      with mrcfile.open(self.list_IDs.iloc[ID, 0], mode='r+') as mrc:
+        mrc.voxel_size = 1.0
         volume = mrc.data
-      
-      length = volume.shape[0]
+
+      # normalise
+      array_max = np.max(map_array)
+      array_min = np.min(map_array)
+      diff = array_max - array_min
+      volume_normed = ((volume - array_min) / diff)#map_array
+
+      volume_normed[volume_normed < 0] = 0
+      volume_normed[volume_normed > 1] = 1
+
+      length = volume_normed.shape[0]
       edited_volume = np.zeros((length, length, length))
-      # rotation around x-axis
-      deg1 = np.random.choice(90, 1, replace=False)[0]
-      for slice_num in range(volume.shape[0]):
-        #print("Working on slice number: ", slice_num)
-        # Get slice
-        slice = volume[slice_num, :, :]
-        # Scale slice
-        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
-        # Round to the nearest integer
-        slice_scaled_int = np.rint(slice_scaled)
-        # do data augmentation as rotation for a random angle between 0 and 90 deg
-        # for all even numbers in the total image stack
-        # check that the remainder of division is 0 and hence the result even
-        # get a random number between 0 and 90 deg
-        # rotate the slice by this deg
-        slice_scaled_int = rotate(slice_scaled_int, angle = deg1, reshape=False)
-        # combine the slices to a new image stack for training
-        edited_volume[slice_num, :, :] = slice_scaled_int
+      # rotate entire volume
+      deg = np.random.choice(90, 1, replace=False)[0]
+      volume_rot = ndimage.rotate(volume_normed, deg, reshape=False)
 
-      # rotation around y-axis
-      deg2 = np.random.choice(90, 1, replace=False)[0]
-      for slice_num in range(volume.shape[1]):
-        #print("Working on slice number: ", slice_num)
-        # Get slice
-        slice = volume[:, slice_num, :]
-        # Scale slice
-        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
-        # Round to the nearest integer
-        slice_scaled_int = np.rint(slice_scaled)
-        # do data augmentation as rotation for a random angle between 0 and 90 deg
-        # for all even numbers in the total image stack
-        # check that the remainder of division is 0 and hence the result even
-        # get a random number between 0 and 90 deg
-        # rotate the slice by this deg
-        slice_scaled_int = rotate(slice_scaled_int, angle = deg2, reshape=False)
-        # combine the slices to a new image stack for training
-        edited_volume[:, slice_num, :] = slice_scaled_int
-
-      # rotation around z-axis
-      deg3 = np.random.choice(90, 1, replace=False)[0]
-      for slice_num in range(volume.shape[1]):
-        #print("Working on slice number: ", slice_num)
-        # Get slice
-        slice = volume[:, :, slice_num]
-        # Scale slice
-        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
-        # Round to the nearest integer
-        slice_scaled_int = np.rint(slice_scaled)
-        # do data augmentation as rotation for a random angle between 0 and 90 deg
-        # for all even numbers in the total image stack
-        # check that the remainder of division is 0 and hence the result even
-        # get a random number between 0 and 90 deg
-        # rotate the slice by this deg
-        slice_scaled_int = rotate(slice_scaled_int, angle = deg3, reshape=False)
-        # combine the slices to a new image stack for training
-        edited_volume[:, :, slice_num] = slice_scaled_int
+#      # rotation around x-axis
+#      deg1 = np.random.choice(90, 1, replace=False)[0]
+#      for slice_num in range(volume.shape[0]):
+#        #print("Working on slice number: ", slice_num)
+#        # Get slice
+#        slice = volume[slice_num, :, :]
+#        # Scale slice
+#        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
+#        # Round to the nearest integer
+#        slice_scaled_int = np.rint(slice_scaled)
+#        # do data augmentation as rotation for a random angle between 0 and 90 deg
+#        # for all even numbers in the total image stack
+#        # check that the remainder of division is 0 and hence the result even
+#        # get a random number between 0 and 90 deg
+#        # rotate the slice by this deg
+#        slice_scaled_int = rotate(slice_scaled_int, angle = deg1, reshape=False)
+#        # combine the slices to a new image stack for training
+#        edited_volume[slice_num, :, :] = slice_scaled_int
+#
+#      # rotation around y-axis
+#      deg2 = np.random.choice(90, 1, replace=False)[0]
+#      for slice_num in range(volume.shape[1]):
+#        #print("Working on slice number: ", slice_num)
+#        # Get slice
+#        slice = volume[:, slice_num, :]
+#        # Scale slice
+#        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
+#        # Round to the nearest integer
+#        slice_scaled_int = np.rint(slice_scaled)
+#        # do data augmentation as rotation for a random angle between 0 and 90 deg
+#        # for all even numbers in the total image stack
+#        # check that the remainder of division is 0 and hence the result even
+#        # get a random number between 0 and 90 deg
+#        # rotate the slice by this deg
+#        slice_scaled_int = rotate(slice_scaled_int, angle = deg2, reshape=False)
+#        # combine the slices to a new image stack for training
+#        edited_volume[:, slice_num, :] = slice_scaled_int
+#
+#      # rotation around z-axis
+#      deg3 = np.random.choice(90, 1, replace=False)[0]
+#      for slice_num in range(volume.shape[1]):
+#        #print("Working on slice number: ", slice_num)
+#        # Get slice
+#        slice = volume[:, :, slice_num]
+#        # Scale slice
+#        slice_scaled = ((slice - slice.min()) / (slice.max() - slice.min())) * 255.0
+#        # Round to the nearest integer
+#        slice_scaled_int = np.rint(slice_scaled)
+#        # do data augmentation as rotation for a random angle between 0 and 90 deg
+#        # for all even numbers in the total image stack
+#        # check that the remainder of division is 0 and hence the result even
+#        # get a random number between 0 and 90 deg
+#        # rotate the slice by this deg
+#        slice_scaled_int = rotate(slice_scaled_int, angle = deg3, reshape=False)
+#        # combine the slices to a new image stack for training
+#        edited_volume[:, :, slice_num] = slice_scaled_int
 
       # Store sample
-      X[i,] = edited_volume.reshape(*self.dim, self.n_channels)
+#      X[i,] = edited_volume.reshape(*self.dim, self.n_channels)
+      X[i,] = volume_rot.reshape(*self.dim, self.n_channels)
 
       y[i] = self.labels[ID]
 
