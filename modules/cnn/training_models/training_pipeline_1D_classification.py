809c46d4c62dee7e5e401d2e7807230ff902afc4
mevol <melanie.vollmar@diamond.ac.uk>
2022-05-24 13:39:26 +0100

    one more

--- modules/cnn/training_models/training_pipeline_1D_classification.py
+++ modules/cnn/training_models/training_pipeline_1D_classification.py
@@ -101,7 +101,7 @@ def pipeline(create_model: Callable[[int, int], Model], parameters_dict: dict):
 
   #MAP_DIM = tuple(np.zeros((length, length, length)).reshape(-1))[0]
   #MAP_DIM = len(np.zeros((length, length, length)).reshape(-1))
-  MAP_DIM = np.zeros((length, length, length)).ravel().shape
+  MAP_DIM = np.zeros((length, length, length)).ravel().shape[0]
   
   print("Shape of flattened target array: ", MAP_DIM)
 
